6f714506a5202a165d1b91a2fcd5c71139899cda
Leonardo Coelho Negri <leonardocoelhonegri@gmail.com>
2022-04-10 11:29:23 -0300

    first commmit

---
+++ src/main/java/com/negri/lgbtrights/LgbtRightsApplication.java
@@ -0,0 +1,13 @@
+package com.negri.lgbtrights;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LgbtRightsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LgbtRightsApplication.class, args);
+	}
+
+}
